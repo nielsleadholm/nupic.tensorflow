c288ba18f3ffcdd7d2c99ed4ca4f90410970c997
Luiz Scheinkman <lscheinkman@numenta.com>
2019-08-21 16:56:20 -0700

    Update models with image data format

--- nupic/tensorflow/models/sparse_cnn.py
+++ nupic/tensorflow/models/sparse_cnn.py
@@ -17,11 +17,10 @@
 #
 #  http://numenta.org/licenses/
 #
-import tensorflow as tf
-from tensorflow import keras
-
 from nupic.tensorflow.constraints import SparseWeights
 from nupic.tensorflow.layers import KWinners, KWinners2d
+from tensorflow import keras
+from tensorflow.python.keras import backend as K
 
 
 class GSCSparseCNN(keras.Sequential):
@@ -43,6 +42,12 @@ class GSCSparseCNN(keras.Sequential):
     :param boost_strength: boost strength (0.0 implies no boosting)
     :param boost_strength_factor: Boost strength factor to use [0..1]
     :param duty_cycle_period: The period used to calculate duty cycles
+    :param data_format:
+        one of `channels_first` or `channels_last`. The ordering of
+        the dimensions in the inputs. `channels_last` corresponds to inputs with
+        shape `(batch, height, width, channels)` while `channels_first` corresponds
+        to inputs with shape `(batch, channels, height, width)`.
+        Similar to `data_format` argument in :class:`keras.layers.Conv2D`.
     """
 
     def __init__(
@@ -56,58 +61,78 @@ class GSCSparseCNN(keras.Sequential):
         boost_strength_factor=0.9,
         k_inference_factor=1.5,
         duty_cycle_period=1000,
+        data_format=K.image_data_format(),
         **kwargs,
     ):
         super(GSCSparseCNN, self).__init__(**kwargs)
 
+        if data_format == "channels_first":
+            axis = 1
+            input_shape = (1, 32, 32)
+        else:
+            axis = -1
+            input_shape = (32, 32, 1)
+
         self.add(
             keras.layers.Conv2D(
                 name="cnn1",
-                data_format="channels_first",
-                input_shape=(1, 32, 32),
+                data_format=data_format,
+                input_shape=input_shape,
                 filters=cnn_out_channels[0],
                 kernel_size=5,
             )
         )
         self.add(keras.layers.BatchNormalization(name="cnn1_batchnorm",
-                                                 axis=1,
-                                                 trainable=False))
+                                                 axis=axis,
+                                                 epsilon=1e-05,
+                                                 momentum=0.9,
+                                                 center=False,
+                                                 scale=False))
         self.add(keras.layers.MaxPool2D(name="cnn1_maxpool",
-                                        data_format="channels_first"))
+                                        pool_size=2,
+                                        padding="same",
+                                        data_format=data_format))
         self.add(
             KWinners2d(
                 name="cnn1_kwinner",
+                data_format=data_format,
                 percent_on=cnn_percent_on[0],
+                k_inference_factor=k_inference_factor,
                 boost_strength=boost_strength,
                 boost_strength_factor=boost_strength_factor,
-                k_inference_factor=k_inference_factor,
                 duty_cycle_period=duty_cycle_period,
             )
         )
         self.add(
             keras.layers.Conv2D(
                 name="cnn2",
-                data_format="channels_first",
+                data_format=data_format,
                 filters=cnn_out_channels[1],
                 kernel_size=5,
             )
         )
         self.add(keras.layers.BatchNormalization(name="cnn2_batchnorm",
-                                                 axis=1,
-                                                 trainable=False))
+                                                 axis=axis,
+                                                 epsilon=1e-05,
+                                                 momentum=0.9,
+                                                 center=False,
+                                                 scale=False))
         self.add(keras.layers.MaxPool2D(name="cnn2_maxpool",
-                                        data_format="channels_first"))
+                                        pool_size=2,
+                                        padding="same",
+                                        data_format=data_format))
         self.add(
             KWinners2d(
                 name="cnn2_kwinner",
+                data_format=data_format,
                 percent_on=cnn_percent_on[1],
+                k_inference_factor=k_inference_factor,
                 boost_strength=boost_strength,
                 boost_strength_factor=boost_strength_factor,
-                k_inference_factor=k_inference_factor,
                 duty_cycle_period=duty_cycle_period,
             )
         )
-        self.add(keras.layers.Flatten(name="flatten"))
+        self.add(keras.layers.Flatten(name="flatten", data_format=data_format))
         self.add(
             keras.layers.Dense(
                 name="linear",
@@ -116,18 +141,22 @@ class GSCSparseCNN(keras.Sequential):
             )
         )
         self.add(keras.layers.BatchNormalization(name="linear_bn",
-                                                 trainable=False))
+                                                 epsilon=1e-05,
+                                                 momentum=0.9,
+                                                 center=False,
+                                                 scale=False))
         self.add(
             KWinners(
                 name="linear_kwinner",
                 percent_on=linear_percent_on,
+                k_inference_factor=k_inference_factor,
                 boost_strength=boost_strength,
                 boost_strength_factor=boost_strength_factor,
-                k_inference_factor=k_inference_factor,
                 duty_cycle_period=duty_cycle_period,
             )
         )
-        self.add(keras.layers.Dense(name="output", units=12, activation=tf.nn.softmax))
+        self.add(keras.layers.Dense(name="output", units=12))
+        self.add(keras.layers.Softmax(axis=1))
 
 
 class GSCSuperSparseCNN(GSCSparseCNN):
@@ -139,9 +168,10 @@ class GSCSuperSparseCNN(GSCSparseCNN):
 
     """
 
-    def __init__(self):
+    def __init__(self, data_format=K.image_data_format()):
         super(GSCSuperSparseCNN, self).__init__(
             linear_units=1500,
             linear_percent_on=0.067,
             linear_weight_sparsity=0.1,
+            data_format=data_format,
         )
